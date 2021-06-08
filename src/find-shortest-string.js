3a8abab67082351ac2ed84d581fc9035bf570ac0
Carrie <carriearoberts95@gmail.com>
2021-06-09 00:04:54 +0100

    attempt 4

--- src/find-shortest-string.js
+++ src/find-shortest-string.js
@@ -17,9 +17,9 @@ function findShortestString(stringArr) {
   return shortestString;
 }
 
-console.log(findShortestString(["it", "is", "a", "nice", "day"]), "a");
-console.log(findShortestString(["why", "hello", "to", "you"]), "to");
-console.log(findShortestString(["brave", "dance"]), "brave");
+// console.log(findShortestString(["it", "is", "a", "nice", "day"]), "a");
+// console.log(findShortestString(["why", "hello", "to", "you"]), "to");
+// console.log(findShortestString(["brave", "dance"]), "brave");
 
 // export function so it can be used in test file
 module.exports = findShortestString;
